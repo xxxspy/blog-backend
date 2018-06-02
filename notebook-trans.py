65bccc87f99bd824ec27738b06f39a6ab727035c
xxxspy <xxxspy@126.com>
2018-06-02 23:05:37 +0800

    new

--- notebook-trans.py
+++ notebook-trans.py
@@ -154,5 +154,5 @@ def to_hexo(notebook_file, with_images=True, with_assets=False):
 
 
 if __name__ == '__main__':
-    notebook_file = r'D:\mysites\notebooks\[转]吊炸天的中文自然语言处理工具和语料库介绍.ipynb'
+    notebook_file = r'D:\mysites\notebooks\大数据揭示心理学规律(利用gensim训练词向量).ipynb'
     to_hexo(notebook_file)
