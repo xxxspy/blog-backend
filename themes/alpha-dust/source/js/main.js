1252b39fc0bf18c0f1d98d65bb895585b3df4685
孙亚东 <xxxspy@126.com>
2018-07-18 20:21:21 +0800

    remove changyan add weibo

--- themes/alpha-dust/source/js/main.js
+++ themes/alpha-dust/source/js/main.js
@@ -59,6 +59,85 @@ var alphaDust = function () {
         });
     }
 
+    function  _hideWeibo() {
+        let weibo = $('#weibo-container')
+        weibo.hide()
+        
+    }
+
+    function _openWeibo(){
+        $('.menu-bg').show();
+        TweenLite.to('.menu-bg', 1, { opacity: '0.92' });
+        let weibo = $('#weibo-container')
+        let width = $(window).width()
+        let weiboWidth = 560
+        let left = 0
+        if (width > weiboWidth){
+            left = (width - weiboWidth)/2
+        }
+        weibo.show()
+        weibo.css({
+            'z-index':1000, 
+            display: 'block',
+            height: 1100,
+            width: 650,
+            top: 150,
+            left: left,
+            position: 'fixed',
+            overflow: 'scroll'
+        })
+        let menuBack = $('.menu-bg')
+        menuBack.click(function () {
+            $('.menu-bg').hide();
+            $('.menu-bg').css({ opacity: '0' })
+            weibo.hide()
+        })
+        return
+
+        let menu = $('.menu-container')
+        menu.hide()
+        let script = `
+        <script src="//tjs.sjs.sinajs.cn/open/api/js/wb.js" type="text/javascript" charset="utf-8"></script>
+        `
+        $('head').append(script)
+        let html = `#document
+                    <html xmlns:wb="http://open.weibo.com/wb">
+                        <head>
+                            ${script}
+                        </head>
+                        <body>
+                            <wb:topic column="n" border="n" 
+                                width="${width}" height="1190" tags="<%= item.title %>" language="zh_cn" version="base"
+                                appkey="4ljXPc" footbar="y" url="<%- url_for(item.path) %>" filter="y" 
+                                color="000000%2Cffffff%2C0078b6%2Ccccccc%2C333333%2Cfafeff%2C0078b6%2Ccccccc%2Ccccccc%2Ce9f4fb00">
+                            </wb:topic>
+                        </body>
+                    </html>`
+        let iframe = `<iframe data-content="${html}">
+
+                    </iframe>`
+        let element = `<div id="weibo-container">${script}<p>blablabla</p><wb:topic column="n" border="n" width="${width}" height="1190" tags="hello" language="zh_cn" version="base"
+    appkey="4ljXPc" footbar="y" url="http://mlln.cn" filter="y" color="000000%2Cffffff%2C0078b6%2Ccccccc%2C333333%2Cfafeff%2C0078b6%2Ccccccc%2Ccccccc%2Ce9f4fb00"></wb:topic></div>`
+        menuBack.append(element)
+        menuBack.click(function(){
+            $('#weibo-container').remove()
+            $('.menu-bg').hide();
+            $('.menu-bg').css({opacity: '0'})
+            menu.show()
+        })
+    }
+
+    function initWeibo() {
+        if($('#weibo-open-btn').length){
+            _hideWeibo()
+            $('#weibo-open-btn').click(function(){
+                _openWeibo()
+                
+            })
+
+        }
+    }
+
     function displayArchives() {
         $('.archive-post').css({opacity: 0});
         TweenMax.staggerTo('.archive-post', 0.4, {opacity: 1}, 0.15);
@@ -67,7 +146,8 @@ var alphaDust = function () {
     return {
         initPostHeader: initPostHeader,
         initMenu: initMenu,
-        displayArchives: displayArchives
+        displayArchives: displayArchives,
+        initWeibo: initWeibo
     };
 }();
 
@@ -76,6 +156,7 @@ $(document).ready(function () {
     alphaDust.initPostHeader();
     alphaDust.initMenu();
     alphaDust.displayArchives();
+    alphaDust.initWeibo();
 });
 
 
