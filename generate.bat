8dabf1487aca280a66c1a88385b7644cb01770df
xxxspy <xxxspy@126.com>
2025-06-16 16:43:25 +0800

    Update generated site

---
+++ generate.bat
@@ -0,0 +1,12 @@
+call hexo clean
+call hexo g
+echo step1
+xcopy /E /I /Y .public\*.* ..\xxxspy.github.io\
+git add .
+git commit -m "Update generated site"
+git push
+cd xxxspy.github.io
+git add .
+git commit -m "Update generated site"
+git push
+pause
